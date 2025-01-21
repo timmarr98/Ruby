6afe403115bf37052958f3cb0dee1cd107e67f46
Timothy <timohty.marr@cardflight.com>
2025-01-21 11:44:14 -0500

    initializing ruby learning repo

---
+++ flicks.rb/flicks.rb
@@ -0,0 +1,6 @@
+puts "goonies has a rank of 10"
+
+
+# String interpolation #{Variable}
+#Double quoted string necessary when utilizint string interpolation
+# Single quote strings are for raw strings ONLY 
