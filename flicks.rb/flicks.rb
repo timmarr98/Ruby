007532c581a20782dd71013bde7d882df46906b1
Timothy <timohty.marr@cardflight.com>
2025-01-21 11:45:52 -0500

    :)

--- flicks.rb/flicks.rb
+++ flicks.rb/flicks.rb
@@ -4,3 +4,4 @@ puts "goonies has a rank of 10"
 # String interpolation #{Variable}
 #Double quoted string necessary when utilizint string interpolation
 # Single quote strings are for raw strings ONLY 
+ 
