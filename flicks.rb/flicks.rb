07e00dd5c79c6fc6fe59da3e0f9f0a694e1a066d
Timothy <timohty.marr@cardflight.com>
2025-01-29 15:46:47 -0500

    up to section 11 ruby course

--- flicks.rb/flicks.rb
+++ flicks.rb/flicks.rb
@@ -6,6 +6,9 @@ puts "goonies has a rank of 10"
 # Single quote strings are for raw strings ONLY 
  
 class Movie
+
+    attr_reader :title, :rank 
+    attr_writer :title, :rank
     def initialize(title,rank = 5)
         @title = title.capitalize
         @rank = rank
@@ -16,8 +19,13 @@ class Movie
     end
 end
 
+
 movie_1 = Movie.new("May", 5)
 puts movie_1.info
+puts movie_1.title
+puts movie_1.rank
+movie_1.title = "New title"
+puts movie_1.title
 
 movie_2 = Movie.new("Shrek", 12)
 puts movie_2.info
--- studio_game.rb
+++ studio_game.rb
@@ -1,34 +1,87 @@
 class Player
+    attr_reader :health
+    attr_accessor :name
+    
+
     def initialize(name, health = 100)
             @name = name.capitalize
             @health = health
     end
 
     def to_s()
-        return "I'm #{@name} with a health of #{@health}"
+        return "I'm #{@name} with a health of #{@health} and a score of #{score()}"
     end
 
     def drain()
         return @health -= 10
     end
+
+    def score()
+        return @health + @name.length 
+    end
     
     def boost()
         return @health += 15
     end
+
+    def name=(new_name)
+        @name = new_name.capitalize
+    end
+
 end  
 
+# number_rolled = rand(1..6)
+# puts number_rolled
 player1 = Player.new("Player #1", health = 20)
-puts player1
-player1.drain()
-puts player1
-player1.boost()
-puts player1
-
+# puts player1
 
 
 player2 = Player.new("Player #2")
-puts player2
+# puts player2
+# puts player2.health
+# puts player2.name
 
 player3 = Player.new("Player #3", health = 0)
-puts player3
+player4 = Player.new("Player #4", health = 100)
+player5 = Player.new("Cole", health = 75)
+# puts player3
+# puts player3.health
+# player3.name = "new Player #3"
+# puts player3.name
+# puts player3.score
+
+players = [player1, player2, player3, player4]
+players.pop
+players.push(player5)
+puts "before playing"
+puts players
+
+
+players.each do |player|
+    number_rolled = rand(1..6)
+    puts number_rolled
+    case number_rolled 
+    when 1..2
+        player.drain()
+        puts "#{player.name} got drained"
+    when 3..4
+        puts "#{player.name} got skipped"
+    else
+        player.boost()
+        puts "#{player.name} got boosted"
+    end
+end
+
+puts "\nafter playing"
+puts players
+
+
+
+
+
+
+
+
+
+
 
