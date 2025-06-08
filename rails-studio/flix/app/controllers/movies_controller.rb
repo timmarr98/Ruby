f934bd3cadb9e0a256fc9c266129268cef4080db
timmarr98 <tmarr006@fiu.edu>
2025-06-08 13:00:18 -0400

    The Flash

--- rails-studio/flix/app/controllers/movies_controller.rb
+++ rails-studio/flix/app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
     def update
         @movie = Movie.find(params[:id])
         if @movie.update(movie_params)
-            redirect_to @movie
+            redirect_to @movie, notice: 'Movie was successfully updated.'
         else
             render :edit, status: :unprocessable_entity
         end
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
     def create
         @movie = Movie.new(movie_params)
         if @movie.save
-            redirect_to @movie
+            redirect_to @movie, notice: 'Movie was successfully created.'
         else
             render :new, status: :unprocessable_entity
         end
@@ -36,7 +36,7 @@ class MoviesController < ApplicationController
     def destroy
         @movie = Movie.find(params[:id])
         @movie.destroy
-        redirect_to movies_url, status: :see_other
+        redirect_to movies_url, status: :see_other, danger: "I'm sorry, Dave, I'm afraid I can't do that!"
     end
 
     private
