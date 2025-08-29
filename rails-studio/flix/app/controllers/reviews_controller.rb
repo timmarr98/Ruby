48bcb53e588b819302622161c1427281a2f9103a
Timothy <timothy@Timothys-MacBook-Pro.local>
2025-08-29 00:17:58 -0400

    one-to-many:forms

--- rails-studio/flix/app/controllers/reviews_controller.rb
+++ rails-studio/flix/app/controllers/reviews_controller.rb
@@ -1,6 +1,29 @@
 class ReviewsController < ApplicationController
-  def index
+  before_action :set_movie
+
+  def set_movie
     @movie = Movie.find(params[:movie_id])
+  end
+  def index
     @reviews = @movie.reviews
   end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+    if @review.save
+      redirect_to movie_reviews_path, notice: 'Movie was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def review_params
+    return params.require(:review).permit(:name, :comment, :stars)
+  end
+
 end
