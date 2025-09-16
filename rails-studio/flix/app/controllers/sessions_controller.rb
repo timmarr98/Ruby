10065885e3b09c23432ae65d87155b7b689af89f
Timothy <timmymarr@gmail.com>
2025-09-15 21:48:59 -0400

    30 authentication

--- rails-studio/flix/app/controllers/sessions_controller.rb
+++ rails-studio/flix/app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
 
   end
   def create
-    fail
+    user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+    if user && user.authenticate(params[:password])
+      #put user id in session
+      session[:user_id] = user.id
+      redirect_to user, notice: "welcome back. #{user.name}"
+    else
+      flash.now[:alert] = "Invalid email/username or password"
+      render :new, status: :unprocessable_entity
+    end
+
   end
 
   def destroy
